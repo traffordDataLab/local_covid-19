9e9b4bee4644d30873d715093452acca752ff2c4
James Austin <james.austin@trafford.gov.uk>
2023-10-05 11:10:12 +0100

    Updated the population data to use the mid-year 2021 estimates (latest available at this time) and the Local Authorities list to that correct at April 2021 in order to match.

--- data/pre-processing.R
+++ data/pre-processing.R
@@ -1,38 +1,49 @@
 library(tidyverse) ; library(rvest) ; library(httr) ; library(readxl) ; library(janitor) ; library(lubridate) ; library(sf)
 
 # -------------------------------------------
-# Lower Tier Local Authorities
+# Lower Tier Local Authorities (April 2021)
 # -------------------------------------------
 
 # Source: ONS Open Geography Portal
-# URL: https://geoportal.statistics.gov.uk/datasets/lower-tier-local-authority-to-upper-tier-local-authority-april-2019-lookup-in-england-and-wales/data
+# URL: https://geoportal.statistics.gov.uk/datasets/ons::lower-tier-local-authority-to-upper-tier-local-authority-april-2021-lookup-in-england-and-wales-1/about
 
 # NB combine Cornwall and Isles of Scilly 
 
-ltla <- read_csv("https://opendata.arcgis.com/datasets/3e4f4af826d343349c13fb7f0aa2a307_0.csv") %>% 
-  select(area_code = LTLA19CD, area_name = LTLA19NM) %>% 
-  filter(str_detect(area_code, "^E")) %>% 
-  mutate(area_code = case_when(as.character(area_code) %in% c("E06000052", "E06000053") ~ "E06000052", TRUE ~ area_code),
-         area_name = case_when(area_code == "E06000052" ~ "Cornwall and Isles of Scilly", TRUE ~ area_name)) %>% 
-  distinct(area_code, .keep_all = TRUE)
+ltla <- st_read("https://services1.arcgis.com/ESMARspQHYMw9BZ9/arcgis/rest/services/LTLA21_UTLA21_EW_LU_9bbac05558b74a88bda913ad5bf66917/FeatureServer/0/query?outFields=*&where=1%3D1&f=geojson") %>%
+    st_drop_geometry() %>%
+    select(area_code = LTLA21CD, area_name = LTLA21NM) %>% 
+    filter(str_detect(area_code, "^E")) %>% 
+    mutate(area_code = case_when(as.character(area_code) %in% c("E06000052", "E06000053") ~ "E06000052", TRUE ~ area_code),
+           area_name = case_when(area_code == "E06000052" ~ "Cornwall and Isles of Scilly", TRUE ~ area_name)) %>% 
+    distinct(area_code, .keep_all = TRUE)
 
-# Mid-2020 population estimates
+# -------------------------------------------
+# Mid-2021 population estimates
+# -------------------------------------------
+
 # Source: Nomis / ONS
-# URL: https://www.nomisweb.co.uk/datasets/pestsyoala
+# URL: https://www.nomisweb.co.uk/datasets/pestsyoala -> https://www.nomisweb.co.uk/query/construct/summary.asp?reset=yes&mode=construct&dataset=2002&version=0&anal=1&initsel=
+# NOMIS selections:
+#   - Geography: local authorities: district / unitary (as of April 2021) [All]
+#   - Date [2021]
+#   - Age [All Ages]
+#   - Sex [Total]
 
 # NB combine population estimates for Cornwall and Isles of Scilly
 
-population <- read_csv("http://www.nomisweb.co.uk/api/v01/dataset/NM_2002_1.data.csv?geography=1820327937...1820328318&date=latest&gender=0&c_age=200&measures=20100&select=geography_code,obs_value") %>% 
+population <- read_csv("https://www.nomisweb.co.uk/api/v01/dataset/NM_2002_1.data.csv?geography=1811939329...1811939332,1811939334...1811939336,1811939338...1811939428,1811939436...1811939442,1811939768,1811939769,1811939443...1811939497,1811939499...1811939501,1811939503,1811939505...1811939507,1811939509...1811939517,1811939519,1811939520,1811939524...1811939570,1811939575...1811939599,1811939601...1811939628,1811939630...1811939634,1811939636...1811939647,1811939649,1811939655...1811939664,1811939667...1811939680,1811939682,1811939683,1811939685,1811939687...1811939704,1811939707,1811939708,1811939710,1811939712...1811939717,1811939719,1811939720,1811939722...1811939730,1811939757...1811939767&date=latest&gender=0&c_age=200&measures=20100&select=geography_code,obs_value") %>% 
   rename(area_code = GEOGRAPHY_CODE, population = OBS_VALUE) %>% 
   mutate(area_code = case_when(as.character(area_code) %in% c("E06000052", "E06000053") ~ "E06000052", TRUE ~ area_code)) %>% 
-  group_by(area_code) %>% 
+  # The following 2 lines are to get the combined total population for Cornwall and the Isles of Scilly as they will now have the same area_code: E06000052
+  group_by(area_code) %>%
   summarise(population = sum(population))
 
 # join datasets and write as CSV
 
 left_join(ltla, population, by = "area_code") %>% 
   write_csv("ltla.csv")
-
+  
+  
 # -------------------------------------------
 # Registered deaths
 # -------------------------------------------
