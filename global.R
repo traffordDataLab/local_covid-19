1ad6d3b539310f9016b8bddaf8658ec4fc1f2527
James Austin <james.austin@trafford.gov.uk>
2023-12-18 20:40:24 +0000

    Created an archive of the data required by the app via the API from the old PHE coronavirus dashboard that is being decommissioned. If this API fails before we have obtained data from the new UKHSA dashboard, we can use the data archive to keep the app running in the meantime.

--- global.R
+++ global.R
@@ -16,6 +16,11 @@ ltla <- read_csv("data/ltla.csv")
 
 # Source: Public Health England
 # URL: https://coronavirus.data.gov.uk
+# NOTE REGARDING CORONAVIRUS DASHBOARD DECOMMISSIONING:
+#   - The original PHE coronavirus dashboard at https://coronavirus.data.gov.uk is being replaced by https://ukhsa-dashboard.data.gov.uk/topics/covid-19
+#   - Final data update on the original coronavirus dashboard took place on 2023-12-14
+#   - An archive of the data up to 2023-12-14 produced by the API call below was created on 2023-12-18 called "data/phe.csv"
+#   - In the event of the API call below no longer working, replace with: phe <- read_csv("data/phe.csv") until we can obtain the data from the new dashboard
 
 phe <- read_csv("https://api.coronavirus.data.gov.uk/v2/data?areaType=ltla&metric=newCasesByPublishDate&metric=newCasesBySpecimenDate&format=csv") %>% 
   mutate(`date` = as.Date(`date`, format = "%Y-%m-%d")) 
